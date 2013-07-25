3870781ce54a6071cc7129f6cbec80b2e5fb8dae
trila <trila.trila@gmail.com>
2013-07-25 17:13:51 +0300

    about doesn't work

---
+++ SpyCamera/src/com/triladroid/spycamera/aboutactivity.java
@@ -0,0 +1,13 @@
+package com.triladroid.spycamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class aboutactivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
